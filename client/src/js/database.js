a02db54d22f9668bbcee274500b3466048945124
wormlippedstrombie <tysonleethornton@outlook.com>
2023-11-14 19:26:15 +1030

    have added functional code to putDb and getDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,36 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+
+  try {
+    await store.add({ content });
+    console.log('Content added to the database');
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  } finally {
+    await tx.done;
+  }
+};
+
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+
+  try {
+    const allContent = await store.getAll();
+    console.log('All content retrieved from the database:', allContent);
+    return allContent;
+  } catch (error) {
+    console.error('Error getting content from the database:', error);
+  } finally {
+    await tx.done;
+  }
+};
 
 initdb();
